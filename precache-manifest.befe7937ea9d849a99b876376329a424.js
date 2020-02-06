016a9e43896ce881337ddc6e2d0d88aadf9e4703
Huiyu <iownthegame@gmail.com>
2020-02-07 01:44:52 +0800

    Updates

---
+++ precache-manifest.befe7937ea9d849a99b876376329a424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b25d07970d1a1bc9a9cbb7d27148b2",
+    "url": "/face_mask_finder/index.html"
+  },
+  {
+    "revision": "203e317a671246cd6d4a",
+    "url": "/face_mask_finder/static/css/main.5a04d194.chunk.css"
+  },
+  {
+    "revision": "8beee75a2ba5b75a8cad",
+    "url": "/face_mask_finder/static/js/2.f35efde9.chunk.js"
+  },
+  {
+    "revision": "959150e688866e3de05cb62dc4dcd7db",
+    "url": "/face_mask_finder/static/js/2.f35efde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203e317a671246cd6d4a",
+    "url": "/face_mask_finder/static/js/main.d3838a75.chunk.js"
+  },
+  {
+    "revision": "b732a48599caa9419246",
+    "url": "/face_mask_finder/static/js/runtime-main.8285d143.js"
+  }
+]);
